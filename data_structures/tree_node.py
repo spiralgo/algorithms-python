3e8a8814f59a5ee793f7a4e86dcb492174b04724
ErdemT09 <erdemturkoglu40@gmail.com>
2021-05-07 16:09:44 +0300

    207-Python initial

---
+++ data_structures/tree_node.py
@@ -0,0 +1,10 @@
+class TreeNode:
+    val = 0
+    right = None
+    left = None
+    def __init__(self, val, left = None, right = None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
